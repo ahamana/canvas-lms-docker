0a8bab27009e9ee717a959ac544b27c84bde7401
Akihiro Hamana <doarat@gmail.com>
2025-09-18 22:53:17 +0900

    docker-entrypoint.sh の CANVAS_INIT_SCRIPT 変数を追加し、スクリプトのパスを明示化

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ -f ${HOME}/script/canvas_init ]; then
-  ${HOME}/script/canvas_init start
+readonly CANVAS_INIT_SCRIPT="${HOME}/script/canvas_init"
+
+if [ -f ${CANVAS_INIT_SCRIPT} ]; then
+  ${CANVAS_INIT_SCRIPT} start
 fi
 
 exec "$@"
